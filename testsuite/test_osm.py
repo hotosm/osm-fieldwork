2742461c5f56416948bf40e8b2f916f37a662a59
Rob Savoye <rob@senecass.com>
2021-10-11 14:52:46 -0600

    Add uid and user to all the test data

--- testsuite/test_osm.py
+++ testsuite/test_osm.py
@@ -40,9 +40,6 @@ def test_init():
 
 def test_header():
     osm.header()
-    tmp = open("test.osm", 'r')
-    lines = tmp.readlines()
-    print(lines)
     assert os.stat("test.osm").st_size > 0
 
 
@@ -55,8 +52,65 @@ def test_footer():
     assert last == "</osm>\n"
 
 
+def test_create_tag():
+    tmp = osm.createTag('foo', 'bar')
+    assert tmp['foo'] == 'bar'
+
+
+def test_create_node_notags():
+    node = dict(osm_id=12345, lat=1, lon=2, uid=54321, user='bar')
+    tmp = osm.createNode(node).lstrip().split(' ')
+    assert tmp[1] == "id='12345'" and tmp[2] == "version='1'" and tmp[3] == "lat='1'"
+    # print(tmp)
+
+
+def test_create_node_modified():
+    node = dict(osm_id=12345, lat=1, lon=2, version=2, uid=54321, user='bar')
+    tmp = osm.createNode(node, modified=True)
+    assert tmp.find("action")
+    # print(tmp)
+
+
+def test_create_node_tags():
+    node = dict()
+    tags = dict(foo='bar', bar='foo', uid=54321, user='bar')
+    node['osm_id'] = 123456
+    node['lat'] = 2
+    node['lon'] = 3
+    node['user'] = 'foo'
+    node['uid'] = 12345
+    node['tags'] = tags
+    tmp = osm.createNode(node)
+    assert tmp.find('bar') and tmp.find('foo')
+    # print(tmp)
+
+
+def test_create_way():
+    way = dict()
+    tags = dict(foo='bar', bar='foo')
+    refs = list()
+    way['osm_id'] = 123456
+    way['user'] = 'foo'
+    way['uid'] = 12345
+    refs.append(12345)
+    refs.append(23456)
+    refs.append(34567)
+    way['tags'] = tags
+    way['refs'] = refs
+    tmp = osm.createWay(way)
+    # print(tmp)
+
+
 if __name__ == '__main__':
     test_init()
     test_header()
     test_footer()
+    test_create_tag()
+    test_create_node_notags()
+    test_create_node_tags()
+    test_create_way()
+    test_create_node_modified()
+    # Delete the test output file
+    if os.path.exists("test.osm"):
+        os.remove("test.osm")
 
