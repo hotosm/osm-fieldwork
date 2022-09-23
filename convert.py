149f6d50e1bc91421723fbab0ea275cef40e9e66
Rob Savoye <rob@senecass.com>
2022-09-23 09:20:22 -0600

    heavy refactoring to handle multiple output fields in the YAML file

--- convert.py
+++ convert.py
@@ -20,7 +20,9 @@
 
 from yamlfile import YamlFile
 import logging
-
+import epdb
+import argparse
+import sys
 
 class Convert(YamlFile):
     """A class to apply a YAML config file and convert ODK to OSM"""
@@ -28,7 +30,43 @@ class Convert(YamlFile):
         if xform is None:
             xform = "xforms.yaml"
         self.yaml = YamlFile(xform)
-        # self.yaml.dump()
+        self.filespec = xform
+        # Parse the file contents into a data structure to make it
+        # easier to retrieve values
+        self.convert = dict()
+        self.ignore = list()
+        self.private = list()
+        for item in self.yaml.yaml['convert']:
+            key = list(item.keys())[0]
+            value = item[key]
+            # print("ZZZZ: %r, %r" % (key, value))
+            if type(value) is str:
+                self.convert[key] = value
+            elif type(value) is list:
+                vals = dict()
+                for entry in value:
+                    if type(entry) is str:
+                        # epdb.st()
+                        tag = entry
+                    else:
+                        tag = list(entry.keys())[0]
+                        vals[tag] = entry[tag]
+                self.convert[key] = vals
+        self.ignore = self.yaml.yaml['ignore']
+        self.private = self.yaml.yaml['private']
+        self.multiple = self.yaml.yaml['multiple']
+
+    def privateData(self, keyword):
+        """See is a keyword is in the private data category"""
+        return keyword in self.private
+
+    def convertData(self, keyword):
+        """See is a keyword is in the convert data category"""
+        return keyword in self.convert
+
+    def ignoreData(self, keyword):
+        """See is a keyword is in the convert data category"""
+        return keyword in self.ignore
 
     def escape(self, value):
         """Escape characters like embedded quotes in text fields"""
@@ -44,27 +82,174 @@ class Convert(YamlFile):
             key = self.yaml.getKeyword(value)
         return key
 
-    def convertList(self, values=list()):
-        """Convert a python list of tags"""
-        features = list()
-        entry = dict()
-        for value in values:
-            entry[self.yaml.getKeyword(value)] = value
-            features.append(entry)
-        return features
+    def getValues(self, tag=None):
+        """Get the values for a primary key"""
+        if tag is not None:
+            if tag in self.convert:
+                return self.convert[tag]
+        else:
+            return None
 
-    def convertTag(self, tag, value):
+    # def convertList(self, values=list()):
+    #     """Convert a python list of tags"""
+    #     features = list()
+    #     entry = dict()
+    #     for value in values:
+    #         entry[self.yaml.getKeyword(value)] = value
+    #         features.append(entry)
+    #     return features
+
+    def convertEntry(self, tag=None, value=None):
+        """Convert a tag and value from the ODK represention to an OSM one"""
+        all = list()
+
+        # If it's not in any conversion data, pass it through unchanged.
+        if tag not in self.convert and tag not in self.ignore and tag not in self.private:
+            return tag, value
+
+        newtag = None
+        newval = None
+        # If the tag is in the config file, convert it.
+        if self.convertData(tag):
+            newtag = self.convertTag(tag)
+            if newtag != tag:
+                logging.debug("Converted Tag for entry \'%s\' to \'%s\'" % (tag, newtag))
+
+        newval = self.convertValue(newtag, value)
+        if newval != value:
+            logging.debug("Converted Value for entry \'%s\' to \'%s\'" % (value, newval))
+
+        # if newtag not in self.tags:
+        #     tmp = { newtag: None }
+        #     key = list()
+        #     all.append(tmp)
+        # else:
+        #     val = self.tags[newtag]
+        #     key = list(val.keys())[0]
+        #     logging.debug("Converted Value for entry %s to %s" % (tag, value))
+        #     # all = self.Value(tag, value)
+        all.append( { newtag: newval } )
+        return newtag, newval
+
+    def convertValue(self, tag=None, value=None):
+        """Convert a single tag value"""
+        all = list()
+
+        vals = self.getValues(tag)
+        # There is no conversion data for this tag
+        if vals is None:
+            return value
+
+        if type(vals) is dict:
+            if value not in vals:
+                all.append({ tag: value })
+                return all
+            if type(vals[value]) is bool:
+                entry = dict()
+                if vals[value]:
+                    entry[tag] = "yes"
+                else:
+                    entry[tag] = "no"
+                all.append(entry)
+                return all
+            for item in vals[value].split(","):
+                entry = dict()
+                tmp =  item.split("=")
+                if len(tmp) == 1:
+                    entry[tag] = vals[value]
+                else:
+                    entry[tmp[0]] =  tmp[1]
+                    logging.debug("\tValue %s converted to %s" % (value, entry))
+                all.append(entry)
+        return all
+
+    def convertTag(self, tag=None):
         """Convert a single tag"""
-        values = self.yaml.getValues("convert")
-        if tag in values:
-            val = values[tag]
-            if type(val) == str:
-                logging.debug("\tTag %s converted to %s" % (tag, val))
+        if tag in self.convert:
+            newtag = self.convert[tag]
+            if type(newtag) is str:
+                logging.debug("\tTag \'%s\' converted to \'%s\'" % (tag, newtag))
+                tmp = newtag.split("=")
+                if len(tmp) > 1:
+                    newtag = tmp[0]
+            elif type(newtag) is list:
+                logging.error("FIXME: list()")
+                # epdb.st()
+                return tag
+            elif type(newtag) is dict:
+                # logging.error("FIXME: dict()")
+                return tag
+            return newtag
+        else:
+            return tag
+
+    def dump(self):
+        """Dump the contents of the yaml file"""
+        print("YAML file: %s" % self.filespec)
+        for key, val in self.convert.items():
+            if type(val) is list:
+                print("Tag %s is" % key)
+                for data in val:
+                    print("\t%r" % data)
             else:
-                for item in val:
-                    key = list(item.keys())[0]
-                    if key == value:
-                        tmp = item[key].split("=")
-                        return tmp[0], tmp[1]
-            return val, value
-        return tag, value
+                print("Tag %s is %s" % (key, val))
+
+#
+# This script can be run standalone for debugging purposes. It's easier to debug
+# this way than using pytest,
+#
+if __name__ == '__main__':
+    # Enable logging to the terminal by default
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+
+    parser = argparse.ArgumentParser(description='Read and parse a YAML file')
+    parser.add_argument("-x", "--xform", default="xform.yaml", help="Default Yaml file")
+    parser.add_argument("-i", "--infile", default="XForms/Ruwa/ruwanigerproject_waterpoint_form.csv", help='The YAML input file')
+    args = parser.parse_args()
+
+    # convert = Convert(args.xform)
+    convert = Convert("xforms.yaml")
+    print("-----")
+    # tag = convert.convertTag("waterpoint_seasonal")
+    # entry = convert.convertEntry("waterpoint_seasonal")
+    # print("YY: %r" % entry)
+
+    # print(convert.convertTag("tourism"))
+    # entry = convert.convertEntry("tourism")
+    # print(entry)
+    # value = convert.convertEntry("waterpoint_seasonal")
+    # print(value)
+    # print("===============")
+
+    # tag = convert.convertTag("waterpoint")
+    # print(tag)
+    # value = convert.convertValue("waterpoint", "well")
+    # print(value)
+    # value = convert.convertValue("power", "solar")
+
+    entry = convert.convertEntry("waterpoint", "faucet")
+    for i in entry:
+        print("XX: %r" % i)
+
+    entry = convert.convertEntry("operational_status", "closed")
+    for i in entry:
+        print("XX: %r" % i)
+
+    entry = convert.convertEntry("seasonal", "wet")
+    for i in entry:
+        print("XX: %r" % i)
+
+    entry = convert.convertEntry("seasonal", "rainy")
+    for i in entry:
+        print("XX: %r" % i)
+
+    entry = convert.convertEntry("power", "solar")
+    for i in entry:
+        print("XX: %r" % i)
