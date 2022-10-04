866a18649eb627e8c0e6eefbd6ff5d1be521ac69
Rob Savoye <rob@senecass.com>
2022-10-04 16:22:19 -0600

    Add support to process multiple instance files, and produce only one CSV.

--- odk2csv.py
+++ odk2csv.py
@@ -25,12 +25,15 @@ import sys
 import re
 from collections import OrderedDict
 import csv
+from pathlib import Path
+from datetime import tzinfo, datetime
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Convert ODK XML instance file to CSV format')
     parser.add_argument("-v", "--verbose", nargs="?",const="0", help="verbose output")
-    parser.add_argument("-i","--instance", default="Test.xml", help='The instance file from ODK Collect')
+    parser.add_argument("-i","--instance", help='The instance file(s) from ODK Collect')
+    # parser.add_argument("-d","--directories", help='A local directory pato to instance files.')
     #parser.add_argument("-o","--outfile", default='tmp.csv', help='The output file for JOSM')
     args = parser.parse_args()
 
@@ -45,49 +48,77 @@ if __name__ == '__main__':
         ch.setFormatter(formatter)
         root.addHandler(ch)
 
-    with open(args.instance, 'rb') as file:
-        # Instances are small, read the whole file        
-        xml = file.read(os.path.getsize(args.instance))
-        doc = xmltodict.parse(xml)
-        fields = list()
-        tags = dict()
-        # import epdb; epdb.st()
-        data = doc['data']
-        for i, j in data.items():
-            if j is None:
-                continue
-            print(i, j)
-            pat = re.compile("[0-9.]* [0-9.-]* [0-9.]* [0-9.]*")
-            if pat.match(str(j)):
-                fields.append("lat")
-                gps = j.split(' ')
-                tags["lat"] = gps[0]
-                fields.append("lon")
-                tags["lon"] = gps[1]
-                continue
-            if type(j) == OrderedDict:
-                for ii, jj in j.items():
-                    if jj is None:
-                        continue
-                    if ii[0:1] != "@":
-                        fields.append(ii)
-                    if type(jj) == OrderedDict:
-                        for iii, jjj in jj.items():
-                            if jjj is not None:
-                                fields.append(iii)
-                                tags[iii] = jjj
-                                # print(iii, jjj)
-                    else:
-                        # print(ii, jj)
-                        tags[ii] = jj
-            else:
-                if i[0:1] != "@":
-                    fields.append(i)
-                    tags[i] = j
+    xmlfiles = list()
+    if args.instance.find("*") >= 0:
+        toplevel = Path()
+        for dir in toplevel.glob(args.instance):
+            if dir.is_dir():
+                xml = os.listdir(dir)
+                # There is always only one XML file per instance
+                full = os.path.join(dir, xml[0])
+                xmlfiles.append(full)
+    else:
+        toplevel = Path(args.instance)
+        if toplevel.is_dir():
+            # There is always only one XML file per instance
+            full = os.path.join(toplevel, os.path.basename(toplevel))
+            xmlfiles.append(full + ".xml")
 
-    outfile = args.instance.replace(".xml", ".csv")
+    print(xmlfiles)
+
+    rows = list()
+    for instance in xmlfiles:
+        logging.info("Processing instance file: %s" % instance)
+        with open(instance, 'rb') as file:
+            # Instances are small, read the whole file        
+            xml = file.read(os.path.getsize(instance))
+            doc = xmltodict.parse(xml)
+            fields = list()
+            tags = dict()
+            # import epdb; epdb.st()
+            data = doc['data']
+            for i, j in data.items():
+                if j is None:
+                    continue
+                print(i, j)
+                pat = re.compile("[0-9.]* [0-9.-]* [0-9.]* [0-9.]*")
+                if pat.match(str(j)):
+                    fields.append("lat")
+                    gps = j.split(' ')
+                    tags["lat"] = gps[0]
+                    fields.append("lon")
+                    tags["lon"] = gps[1]
+                    continue
+                if type(j) == OrderedDict:
+                    for ii, jj in j.items():
+                        if jj is None:
+                            continue
+                        if ii[0:1] != "@":
+                            fields.append(ii)
+                        if type(jj) == OrderedDict:
+                            for iii, jjj in jj.items():
+                                if jjj is not None:
+                                    fields.append(iii)
+                                    tags[iii] = jjj
+                                    # print(iii, jjj)
+                        else:
+                            # print(ii, jj)
+                            tags[ii] = jj
+                else:
+                    if i[0:1] != "@":
+                        fields.append(i)
+                        tags[i] = j
+        rows.append(tags)
+
+    print(os.path.dirname(instance))
+    xml = os.path.basename(instance)
+    tmp = xml.split("_")
+    now = datetime.now()
+    timestamp = f'_{now.year}_{now.hour}_{now.minute}'
+    outfile = tmp[0] + timestamp + ".csv"
     with open(outfile, 'w', newline='') as csvfile:
         csv = csv.DictWriter(csvfile, dialect='excel',fieldnames=fields)
         csv.writeheader()
-        csv.writerow(tags)
+        for row in rows:
+            csv.writerow(row)
 
