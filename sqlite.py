f39f9a48697b177110c6ff106a9c36c88493bc11
Rob Savoye <rob.savoye@hotosm.org>
2022-12-19 12:43:21 -0700

    Implement MapTile class

--- sqlite.py
+++ sqlite.py
@@ -44,20 +44,35 @@ import locale
 
 
 class MapTile(object):
-    def __init__(self, x=None, y=None, z=None, blob=None):
+    def __init__(self, x=None, y=None, z=None, filespec=None):
         self.tile = dict()
-        self.tile['x'] = x
-        self.tile['y'] = y
-        self.tile['z'] = z
-        self.blob = blob
+        self.x = x
+        self.y = y
+        self.z = z
+        self.blob = None
+        if not filespec and z:
+            self.filespec = f"{z}/{x}/{y}.png"
+        else:
+            self.filespec = filespec
+            tmp = filespec.split("/")
+            self.z = tmp[0]
+            self.x = tmp[1]
+            self.y = tmp[2].replace(".png", "")
+            
 
+    def addImage(self, filespec=None):
+        if os.path.exists(filespec):
+            size = os.path.getsize(filespec)
+            file = open(filespec, "rb")
+            self.blob = file.read(size)
+        
     def createTile(self, x=None, y=None, z=None, blob=None):
         if x:
-            self.tile['x'] = x
+            self.x = x
         if y:
-            self.tile['y'] = y
+            self.y = y
         if z:
-            self.tile['z'] = z
+            self.z = z
         if blob:
             self.addBlob(blob)
 
@@ -74,19 +89,25 @@ class MapTile(object):
         return out
 
     def dump(self):
-        print(self.x)
-        print(self.y)
-        print(self.z)
+        print("Z: %r" % self.z)
+        print("X: %r" % self.x)
+        print("Y: %r" % self.y)
+        print("Filespec: %s" % self.filespec)
+        if self.blob:
+            print("Tile size is: %d" % len(self.blob))
 
 class DataFile(object):
     def __init__(self, dbname=None):
+        """Handle the sqlite3 database file"""
         self.db = None
         self.cursor = None
         if dbname:
             self.createDB(dbname)
         self.metadata = None
+        self.toplevel = None
     
     def createDB(self, dbname=None):
+        """Create and sqlitedb in either mbtiles or Osman sqlitedb format"""
         suffix = os.path.splitext(dbname)[1]
         
         logging.info("Created database file %s" % dbname)
@@ -96,26 +117,34 @@ class DataFile(object):
             self.cursor.execute("CREATE TABLE tiles (zoom_level integer, tile_column integer, tile_row integer, tile_data blob)")
             self.cursor.execute("CREATE INDEX tiles_idx on tiles (zoom_level, tile_column, tile_row)")
             self.cursor.execute("CREATE TABLE metadata (name text, value text)")
+            # These get populated later
+            name = None
+            description = None
+            bounds = None
             self.cursor.execute("CREATE UNIQUE INDEX metadata_idx  ON metadata (name)")
             self.cursor.execute("INSERT INTO metadata (name, value) VALUES('version', '1.1')")
             self.cursor.execute("INSERT INTO metadata (name, value) VALUES('type', 'baselayer')")
-            self.cursor.execute("INSERT INTO metadata (name, value) VALUES('name', 'fixme')")
-            self.cursor.execute("INSERT INTO metadata (name, value) VALUES('description', 'fixme')")
-            self.cursor.execute("INSERT INTO metadata (name, value) VALUES('bounds', 'fixme')")
+            self.cursor.execute(f"INSERT INTO metadata (name, value) VALUES('name', '{name}')")
+            self.cursor.execute(f"INSERT INTO metadata (name, value) VALUES('description', '{description}')")
+            self.cursor.execute(f"INSERT INTO metadata (name, value) VALUES('bounds', '{bounds}')")
             self.cursor.execute("INSERT INTO metadata (name, value) VALUES('format', 'png')")
         elif suffix == '.sqlitedb':
             # s is always 0
             self.cursor.execute("CREATE TABLE tiles (x int, y int, z int, s int, image blob, PRIMARY KEY (x,y,z,s));")
             self.cursor.execute("CREATE INDEX IND on tiles (x,y,z,s)")
-            # Info is simple "2|4" for example
+            # Info is simple "2|4" for example, it gets populated later
             self.cursor.execute("CREATE TABLE info (maxzoom Int, minzoom Int);")
-            # the metadata is the locale as a string, 'en_US' in out case
+            # the metadata is the locale as a string
             loc = locale.getlocale()[0]
             self.cursor.execute(f"CREATE TABLE  android_metadata ({loc})")
         self.db.commit()
 
-    def writeTile(self):
-        pass
+    def writeTile(self, tile=None):
+        tile.dump()
+        # self.db.execute("INSERT INTO tiles (x, y, z, s, image) VALUES (?, ?, ?, ?, ?)", [tile.x, tile.y, tile.z, 0, sqlite3.Binary(tile.blob)])
+        self.db.execute("INSERT INTO tiles (tile_row, tile_column, zoom_level, tile_data) VALUES (?, ?, ?, ?)", [tile.x, tile.y, tile.z, sqlite3.Binary(tile.blob)])
+
+        self.db.commit()
 
     def readTile(self):
         pass
@@ -139,3 +168,15 @@ if __name__ == '__main__':
         root.addHandler(ch)
 
         outfile = DataFile(args.database)
+        toplevel = "/var/www/html/topotiles/"
+        foo = "15/12744/6874.png"
+        tmp = foo.split("/")
+        z = tmp[0]
+        x = tmp[1]
+        y = tmp[2].replace(".png", "")
+        
+        file = "15/12744/6874.png"
+        tile1 = MapTile(x, y, z)
+        tile2 = MapTile(filespec=file)
+        tile2.addImage(f'{toplevel}/{foo}')
+        outfile.writeTile(tile2)
