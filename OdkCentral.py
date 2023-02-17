27d0eb69e755343fd01d345ea81bb75d08feeb34
Rob Savoye <rob.savoye@hotosm.org>
2023-02-17 13:41:46 -0700

    Return the entire object afrer

--- OdkCentral.py
+++ OdkCentral.py
@@ -127,7 +127,7 @@ class OdkCentral(object):
             result = self.session.post(url, auth=self.auth, json={'name': name}, verify=self.verify)
             # update the internal list of projects
             self.listProjects()
-        return self.findProject(name)
+        return result.json()
 
     def deleteProject(self, project_id: int):
         """Delete a project on an ODK Central server"""
@@ -405,7 +405,7 @@ class OdkForm(OdkCentral):
         else:
             url = f'{self.base}projects/{projectId}/forms/{xmlFormId}'
         print(url)
-        result = self.session.delete(url, auth=self.auth)
+        result = self.session.delete(url, auth=self.auth, verify=self.verify)
         return result
 
     def publishForm(self, projectId=None, xmlFormId=None):
