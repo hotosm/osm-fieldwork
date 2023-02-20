822d12aeacb86dc8b331c08269e57b631ea872a3
sam <sam.woodcock@protonmail.com>
2023-02-20 21:47:02 +0000

    feat: index for xform files, plus path

---
+++ odkconvert/xforms/__init__.py
@@ -0,0 +1,29 @@
+"""Index of XForm file paths."""
+
+import os
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+buildings = f"{path}/buildings.xls"
+cemeteries = f"{path}/cemeteries.xls"
+education = f"{path}/education.xls"
+healthcare = f"{path}/healthcare.xls"
+landuse = f"{path}/landuse.xls"
+municipality = f"{path}/municipality.csv"
+natural = f"{path}/natural.xls"
+place = f"{path}/place.xls"
+religious = f"{path}/religious.xls"
+school = f"{path}/school.xls"
+solidwaste = f"{path}/solidwaste.xlsx"
+toilets = f"{path}/toilets.xls"
+towns = f"{path}/towns.csv"
+transportation = f"{path}/transportation.xls"
+waste_collection = f"{path}/waste_collection.xlsx"
+waste = f"{path}/waste.xls"
+waterpoints = f"{path}/waterpoints.xls"
+waterways = f"{path}/waterways.xls"
+amenities = f"{path}/lib/amenities.csv"
+opening_hours = f"{path}/lib/opening_hours.csv"
+operational_status = f"{path}/lib/operational_status.csv"
+provider = f"{path}/lib/provider.csv"
