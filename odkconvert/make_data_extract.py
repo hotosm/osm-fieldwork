42975379f2a78e31f05659797ae7f55e0c16dd72
Rob Savoye <rob.savoye@hotosm.org>
2023-03-26 09:11:22 +0545

    Use new DatabaseAccess class

--- odkconvert/make_data_extract.py
+++ odkconvert/make_data_extract.py
@@ -61,6 +61,9 @@ log = logging.getLogger(__name__)
 
 class DatabaseAccess(object):
     def __init__(self, dbhost=None, dbname=None):
+        self.dbshell = None
+        self.dbcursor = None
+        self.category = None
         if dbname == "underpass":
             # Authentication data
             self.auth = HTTPBasicAuth(self.user, self.passwd)
@@ -68,20 +71,33 @@ class DatabaseAccess(object):
             # Use a persistant connect, better for multiple requests
             self.session = requests.Session()
             self.url = "https://raw-data-api0.hotosm.org/"
+            self.headers = {"accept": "application/json", "Content-Type": "application/json",
+                        }
         else:
-            self.auth = None
-            self.sesson = None
-            self.url = None
+            logging.info("Opening database connection to: %s" % dbhost)
+            connect = "PG: dbname=" + dbname
+            if dbhost is None or dbhost == "localhost":
+                connect = f"dbname={dbname}"
+            else:
+                connect = f"host={dbhost} dbname={dbname}"
+            try:
+                self.dbshell = psycopg2.connect(connect)
+                self.dbshell.autocommit = True
+                self.dbcursor = self.dbshell.cursor()
+                if self.dbcursor.closed == 0:
+                    logging.info(f"Opened cursor in {dbname}")
+            except Exception as e:
+                logging.error("Couldn't connect to database: %r" % e)
 
     def createJson(self, category, boundary):
         path = xlsforms_path.replace("xlsforms", "data_models")
         file = open(f"{path}/{category}.yaml", "r").read()
         data = yaml.load(file, Loader=yaml.Loader)
 
-        clip = open(boundary, "r")
-        features = dict()
-        geom = geojson.load(clip)
-        features['geometry'] = geom['geometry']
+        # clip = open(boundary, "r")
+        # features = dict()
+        # geom = geojson.load(clip)
+        features['geometry'] = boundary
 
         # The database tables to query
         # if tags exists, then only query those fields
@@ -120,9 +136,9 @@ class DatabaseAccess(object):
             if 'tags' in select:
                 for tag in select['tags']:
                     query += f" {select[tag]} AS {tag}, "
-                query += "osm_id AS id"
+                query += "osm_id AS id, ST_AsEWKT(ST_Centroid(geom)) "
             else:
-                query += " * "
+                query += "osm_id AS id, ST_AsEWKT(ST_Centroid(geom)), tags "
             query += f" FROM {table} "
             where = data['where']
             # if tags exists, then only query those fields
@@ -135,49 +151,7 @@ class DatabaseAccess(object):
             sql.append(query[:-4])
         return sql
 
-class PostgresClient(DatabaseAccess):
-    """Class to handle SQL queries for the categories"""
-
-    def __init__(self, dbhost=None, dbname=None, output=None):
-        """Initialize the postgres handler"""
-        # OutputFile.__init__( self, output)
-        self.boundary = None
-        logging.info("Opening database connection to: %s" % dbhost)
-        if dbhost == "localhost" and dbname != "underpass":
-            connect = "PG: dbname=" + dbname
-            # if dbhost:
-            #     connect += " host=" + dbhost
-            # self.pg = ogr.Open(connect)
-            if dbhost is None or dbhost == "localhost":
-                connect = f"dbname={dbname}"
-            else:
-                connect = f"host={dbhost} dbname={dbname}"
-            try:
-                self.dbshell = psycopg2.connect(connect)
-                self.dbshell.autocommit = True
-                self.dbcursor = self.dbshell.cursor()
-                if self.dbcursor.closed == 0:
-                    logging.info(f"Opened cursor in {dbname}")
-            except Exception as e:
-                logging.error("Couldn't connect to database: %r" % e)
-        else:
-            pass
-
-    def getFeatures(self, boundary=None, filespec=None, category="buildings"):
-        """Extract buildings from Postgres"""
-        logging.info("Extracting features from Postgres...")
-
-        config = self.createSQL(category)
-
-        if type(boundary) != dict:
-            clip = open(boundary, "r")
-            geom = geojson.load(clip)
-            poly = geom["geometry"]
-        else:
-            poly = boundary
-        config = self.createJson(category, boundary)
-        wkt = shape(poly)
-
+    def queryLocal(self, query, wkt):
         sql = f"DROP VIEW IF EXISTS ways_view;CREATE TEMP VIEW ways_view AS SELECT * FROM ways_poly WHERE ST_CONTAINS(ST_GeomFromEWKT('SRID=4326;{wkt.wkt}'), geom)"
         self.dbcursor.execute(sql)
         sql = f"DROP VIEW IF EXISTS nodes_view;CREATE TEMP VIEW nodes_view AS SELECT * FROM nodes WHERE ST_CONTAINS(ST_GeomFromEWKT('SRID=4326;{wkt.wkt}'), geom)"
@@ -186,30 +160,71 @@ class PostgresClient(DatabaseAccess):
         sql = f"DROP VIEW IF EXISTS relations_view;CREATE TEMP VIEW relations_view AS SELECT * FROM nodes WHERE ST_CONTAINS(ST_GeomFromEWKT('SRID=4326;{wkt.wkt}'), geom)"
         # self.dbcursor.execute(sql)
 
+        if query.find(" ways_poly ") > 0:
+            query = query.replace("ways_poly", "ways_view")
+        elif query.find(" nodes ") > 0:
+            query = query.replace("nodes", "nodes_view")
         features = list()
-        for table in tables:
-            logging.debug("Querying table %s with conditional %s" % (table, filter))
-            query = f"SELECT {select} FROM {table} WHERE {filter}"
-            self.dbcursor.execute(query)
-            result = self.dbcursor.fetchall()
-            logging.info("Query returned %d records" % len(result))
-            for item in result:
-                gps = item[0][16:-1].split(" ")
-                poi = Point((float(gps[0]), float(gps[1])))
-                tags = item[2]
-                tags["id"] = item[1]
-                if "name" in tags:
-                    tags["title"] = tags["name"]
-                    tags["label"] = tags["name"]
-                else:
-                    tags["title"] = None
-                    tags["label"] = None
-                features.append(Feature(geometry=poi, properties=tags))
-        collection = FeatureCollection(features)
+        log.debug(query)
+        self.dbcursor.execute(query)
+        result = self.dbcursor.fetchall()
+        logging.info("Query returned %d records" % len(result))
+        for item in result:
+            gps = item[1][16:-1].split(" ")
+            poi = Point((float(gps[0]), float(gps[1])))
+            tags = item[2]
+            tags["id"] = item[1]
+            if "name" in tags:
+                tags["title"] = tags["name"]
+                tags["label"] = tags["name"]
+            else:
+                tags["title"] = None
+                tags["label"] = None
+            features.append(Feature(geometry=poi, properties=tags))
+        return features
+
+    def queryRemote(self, query):
+        result = self.session.post(self.url, auth=self.auth, data=query, headers=self.headers)
+        return result
+
+class PostgresClient(DatabaseAccess):
+    """Class to handle SQL queries for the categories"""
+    def __init__(self, dbhost=None, dbname=None, output=None):
+        """Initialize the postgres handler"""
+        # OutputFile.__init__( self, output)
+        self.boundary = None
+        super().__init__(dbhost, dbname)
+
+    def getFeatures(self, boundary=None, filespec=None, category="buildings"):
+        """Extract buildings from Postgres"""
+        logging.info("Extracting features from Postgres...")
+
+        if type(boundary) != dict:
+            clip = open(boundary, "r")
+            geom = geojson.load(clip)
+            poly = geom["geometry"]
+        else:
+            poly = boundary
+        wkt = shape(poly)
+
+        if self.dbshell:
+            features = list()
+            sql = self.createSQL(category)
+            for query in sql:
+                result = self.queryLocal(query, wkt)
+            collection = FeatureCollection(result)
+        else:
+            request = self.createJson(category, poly)
+            self.queryRemote(request)
 
         cleaned = FilterData()
         models = xlsforms_path.replace("xlsforms", "data_models")
-        cleaned.parse(f"{models}/Impact Areas - Data Models V1.1.xlsx")
+        # cleaned.parse(f"{models}/Impact Areas - Data Models V1.1.xlsx")
+        file = f"{xlsforms_path}/{category}.xls"
+        if os.path.exists(file):
+            cleaned.parse(f"{xlsforms_path}/{category}.xls")
+        else:
+            cleaned.parse(f"{file}x")
         new = cleaned.cleanData(collection)
         json = open(filespec, "w")
         dump(new, json)
