28f989f9f47f604daa2d51a0878020cff1b054b8
Rob Savoye <rob@senecass.com>
2023-05-27 17:53:56 -0600

    fix: Added Osmand extensions for styling the favorites

--- osm_fieldwork/osm2favorities.py
+++ osm_fieldwork/osm2favorities.py
@@ -41,23 +41,20 @@ def createExtension(icon):
     # trailhead.png, tourism_picnic_site.png, tourism_picnic_site.png,
     # tourism_attraction.png, tourism_information.png, information_board.png,
     # firepit.png, historic_ruins.png, amenity_drinking_water.png,
-    # amenity_toilets.png
-    # amenity_parking.png
-
-
-    # template = f"""<extensions><osmand:icon>{icon}</osmand:icon>
-    # <osmand:background>circle</osmand:background>
-    # <osmand:color>#ff5020</osmand:color>
-    # </extensions>
-    # """
+    # amenity_toilets.png, amenity_parking.png, special_trekking
+    colors = {'tourism_camp_site': '#ff5020', 'tourism_picnic_site': '#ff5020', 'special_trekking': '#a71de1'}
     nsmap = {'osmand': 'https://osmand.net'}
     png = etree.Element('{osmand}icon', nsmap=nsmap)
     png.text = icon
     back = etree.Element('{osmand}background', nsmap=nsmap)
     back.text = "circle"
-    color = etree.Element('{osmand}color', nsmap=nsmap)
-    color.text = '#ff5020'
-    return (png, back, color)
+    color = None
+    if icon in colors:
+        color = etree.Element('{osmand}color', nsmap=nsmap)
+        color.text = colors[icon]
+        return (png, back, color)
+    else:
+        return (png, back)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -98,6 +95,15 @@ if __name__ == "__main__":
             name = ""
             if 'name' in feature['properties']:
                 name = feature['properties']['name']
+                tourism = None
+                if 'tourism' in feature['properties']:
+                    tourism = feature['properties']['tourism']
+                highway = None
+                if 'highway' in feature['properties']:
+                    highway = feature['properties']['highway']
+                amenity = None
+                if 'amenity' in feature['properties'] and not highway:
+                    amenity = feature['properties']['amenity']
             for key, value in feature['properties'].items():
                 if key == 'name':
                     continue
@@ -112,7 +118,15 @@ if __name__ == "__main__":
                 # symbol="",
                 # comment="",
             )
-            extensions = createExtension("tourism_camp_site")
+            if tourism and tourism != 'picnic site':
+                extensions = createExtension("tourism_camp_site")
+            elif tourism and tourism != 'picnic site':
+                extensions = createExtension("tourism_picnic_site")
+            elif highway and highway == 'trailhead':
+                extensions = createExtension("special_trekking")
+            elif amenity and amenity == 'parking':
+                extensions = createExtension("amenity_parking")
+
             for ext in extensions:
                 way.extensions.append(ext)
             gpx.waypoints.append(way)
