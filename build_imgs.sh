75b28a4d697a75b891dce4c7617ab517a6547e16
sam <sam.woodcock@protonmail.com>
2023-08-25 07:02:38 +0100

    build: separate push from ci img build

--- build_imgs.sh
+++ build_imgs.sh
@@ -4,12 +4,16 @@ set -eo pipefail
 
 PKG_VERSION=0.3.5
 
-docker build . --push \
+docker build . \
     -t "ghcr.io/hotosm/osm-fieldwork:${PKG_VERSION}" \
     --target prod \
     --build-arg PKG_VERSION="${PKG_VERSION}"
 
-docker build . --push \
+docker push "ghcr.io/hotosm/osm-fieldwork:${PKG_VERSION}"
+
+docker build . \
     -t ghcr.io/hotosm/osm-fieldwork:ci \
     --target ci \
     --build-arg PKG_VERSION="${PKG_VERSION}"
+
+docker push ghcr.io/hotosm/osm-fieldwork:ci
