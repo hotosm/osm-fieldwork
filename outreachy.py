a675b7000733232dc368f0ae1797e1e7c80218da
siva224513 <mailtosiva2245@gmail.com>
2024-03-14 23:33:24 +0530

    add outreachy.py file(fixes hotosm#204)

---
+++ outreachy.py
@@ -0,0 +1,14 @@
+
+from osm_fieldwork.basemapper import create_basemap_file
+from osm_fieldwork.shared_utils import read_bytes_geojson
+
+FILEPATH = "tests/testdata/Rollinsville.geojson"
+boundary = read_bytes_geojson(FILEPATH)
+
+create_basemap_file(
+    verbose=True,
+    boundary=boundary,
+    outfile="outreachy.mbtiles",
+    zooms="12-15",
+    source="esri",
+)
