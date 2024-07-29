57515d3cd0c887192143d20954b35f615b8aa354
spwoodcock <sam.woodcock@protonmail.com>
2024-07-29 17:35:11 +0100

    refactor: remove entity registration form to archived xlsforms

--- osm_fieldwork/xlsforms/__init__.py
+++ osm_fieldwork/xlsforms/__init__.py
@@ -4,7 +4,5 @@ import os
 
 xlsforms_path = os.path.dirname(os.path.abspath(__file__))
 
-entities_registration = f"{xlsforms_path}/entities_registration.xls"
-entities_registration_xml = f"{xlsforms_path}/entities_registration.xml"
-
 buildings = f"{xlsforms_path}/buildings.xls"
+healthcare = f"{xlsforms_path}/health.xls"
