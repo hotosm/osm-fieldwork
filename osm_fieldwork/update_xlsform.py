daf887419d309ed1697a3a27e6e0f307adc17451
Sujan Adhikari <109404840+Sujanadh@users.noreply.github.com>
2024-11-22 23:07:42 +0545

    feat: calculate coordinates of additional in xlsform entity (#312)

--- osm_fieldwork/update_xlsform.py
+++ osm_fieldwork/update_xlsform.py
@@ -201,10 +201,22 @@ def append_select_one_from_file_row(df: pd.DataFrame, entity_name: str) -> pd.Da
         }
     )
 
+    # Prepare the row for calculating coordinates based on the additional entity
+    coordinates_row = pd.DataFrame(
+        {
+            "type": ["calculate"],
+            "name": ["additional_geometry"],
+            "calculation": [f"instance('{entity_name}')/root/item[name=${entity_name}]/geometry"],
+            "label::English(en)": ["additional_geometry"],
+            "label::Swahili(sw)": ["additional_geometry"],
+            "label::French(fr)": ["additional_geometry"],
+            "label::Spanish(es)": ["additional_geometry"],
+        }
+    )
     # Insert the new row into the DataFrame
     top_df = df.iloc[:row_index_to_split_on]
     bottom_df = df.iloc[row_index_to_split_on:]
-    return pd.concat([top_df, additional_row, bottom_df], ignore_index=True)
+    return pd.concat([top_df, additional_row, coordinates_row, bottom_df], ignore_index=True)
 
 
 async def append_mandatory_fields(
